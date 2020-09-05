65cf34b29641fc47bf18b52535e9a0466d2ba45b
Rahul Chauhan <rahuljungchauhan@gmail.com>
2020-09-05 16:37:02 -0400

    - Set up react file - Connect fromt end to the server

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from React
+      </div>
+    )
+  }
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 7070;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// })
 
 app.listen(port, () => {
   console.log(`Listening to http://localhost:${port}`)
