b4944a539a0c0f56bc67ca82c0387f552b8c4f18
Rahul Chauhan <rahuljungchauhan@gmail.com>
2020-09-07 18:13:21 -0400

    - Create a live Bitcoin Price Index Chart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,75 @@
 import React from 'react';
 import axios from 'axios';
+import {Line} from 'react-chartjs-2';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state ={
-
+      dataKey: [],
+      dataValue: []
     };
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  getData(){
+    axios.get(`/bpi`)
+    .then((res) => {
+      let key = Object.keys(res.data.bpi);
+      let value = Object.values(res.data.bpi)
+      this.setState({
+        dataKey: key,
+        dataValue: value,
+      });
+    })
+    .catch((err) => {
+      console.log('Error getitng data from the server', err);
+    });
   }
 
   render() {
+    const state = {
+      labels: this.state.dataKey,
+      datasets: [
+        {
+          label: 'Price',
+          fill: true,
+          lineTension: 0.5,
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: this.state.dataValue
+        }
+      ]
+    }
     return (
       <div>
-        Hello from React
+        <Line
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Bitcoin Price Index of 2020 (USD$)',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+        <div>Powered by CoinDesk</div>
       </div>
-    )
+    );
   }
+
+
+
 }
 
 export default App;
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const moment = require('moment');
 const app = express();
 const port = 7070;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// })
+// var startDate = moment().subtract(10, 'days').format('YYYY-MM-DD');
+var endDate = moment().format('YYYY-MM-DD');
+
+app.get("/bpi", (req, res) => {
+  axios.get(
+    `https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-01-01&end=${endDate}`
+  )
+    .then(response => {
+      res.send(response.data)
+    })
+    .catch(err => console.log(err));
+});
 
 app.listen(port, () => {
   console.log(`Listening to http://localhost:${port}`)
 })
+
